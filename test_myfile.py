713d2e5667bb61f53bc8a495f144d97877813cb8
dubeavik002 <159172128+dubeavik002@users.noreply.github.com>
2024-08-24 15:22:28 -0400

    Update test_myfile.py

--- test_myfile.py
+++ test_myfile.py
@@ -1,6 +1,18 @@
+def test_sum():
+    assert sum(5,6)==11
+def test_multiply():
+    assert multiply(5,6) == 30
+def test_subtract():
+    assert subtract(6,5) ==1
+
+def test_divide():
+    assert divide(10,2) == 5
+
+def test_stringPlusNumber():
+    assert  stringPlusNumber('abcd',str(5)) == 'abcd5'
+
 def sum(a,b):
     return a+b
-
 def multiply(a,b):
     return a*b
 
@@ -14,4 +26,7 @@ def stringPlusNumber(a,b):
     return a+str(b)
 
 print(sum(5,6))
+# print(multiply(5,6))
+# print(subtract(6,5))
+# print(divide(10,2))
 print(stringPlusNumber('abcd',5))
