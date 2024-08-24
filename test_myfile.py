4496281a5d426365681ba710d059e4605d14487a
dubeavik002 <159172128+dubeavik002@users.noreply.github.com>
2024-08-24 15:04:42 -0400

    Create test_myfile.py

---
+++ test_myfile.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+def subtract(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
+
+def stringPlusNumber(a,b):
+    return a+(b)
+
+print(sum(5,6))
+print(stringPlusNumber('abcd',5))
